4fa9ca92af131478ef9c66dee78f045f0df1e8a4
eunja <dnqls2414@naver.com>
2025-02-23 01:47:51 +0900

    로그인 작업중

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../api/axiosInstance";
+
 const Header = () => {
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    axiosInstance
+      .post("/userInfo")
+      .then((res) => {
+        console.log(res);
+        setUserInfo(res.data);
+      })
+      .catch((error) => {
+        console.log("User profile load failed", error);
+        setUserInfo(null); // 로그인 안된 상태
+      });
+  }, []);
+
+  const handleLogout = () => {
+    axiosInstance
+      .post("/logout")
+      .then((res) => {
+        console.log(res);
+        if (res.data === "success") {
+          setUserInfo(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="header">
       <div className="content">
-        <div className="logo"></div>
+        <div className="logo" onClick={(e) => navigate("/")}></div>
         <div className="search-bar">
           <i className="search-icon"></i>
           <input
@@ -12,8 +46,26 @@ const Header = () => {
           />
         </div>
         <div className="side-menu">
-          <p>로그인</p>
-          <p>회원가입</p>
+          {userInfo ? (
+            <>
+              <button className="menu-logIn">마이페이지</button>
+              <button className="menu-membership" onClick={handleLogout}>
+                로그아웃
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="menu-logIn" onClick={() => navigate("/login")}>
+                로그인
+              </button>
+              <button
+                className="menu-membership"
+                onClick={() => navigate("/member")}
+              >
+                회원가입
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
